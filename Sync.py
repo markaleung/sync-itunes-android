512f0bfef5b3a167cbabdb6c25103b9e67d4a47a
Mark Leung <markaleung@gmail.com>
2024-03-05 09:34:51 +0800

    Syng - Remove comments - Not needed because class is simpler now

--- Sync.py
+++ Sync.py
@@ -1,5 +1,4 @@
-import os, unicodedata, tqdm
-
+import os
 import Song, Playlist, Cleaner
 
 class Sync:
@@ -15,26 +14,20 @@ class Sync:
     def copyPlaylists(self):
         self.playlists = Playlist.Manager(config = self.config)
         self.playlists.main()
-    # Clean Up
     def cleanSongs(self):
         self.song_cleaner = Cleaner.Song(config = self.config)
         self.song_cleaner.main()
     def cleanFolders(self):
         self.folder_cleaner = Cleaner.Folder(config = self.config)
         self.folder_cleaner.main()
-    # Check Files
     def checkFiles(self):
         files = [file for path in os.walk(self.config.dest) for file in path[2]]
         assert len(self.config.file_set) == len(files)
         print(len(self.config.file_set), len(files))
     def main(self):
         self.readPlaylist()
-        # Songs
         self.copySongs()
-        # Playlists
         self.copyPlaylists()
-        # Clean Up
         self.cleanSongs()    
         self.cleanFolders()
-        # Check
         self.checkFiles()
